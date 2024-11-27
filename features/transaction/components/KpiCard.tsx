467750dfc7503e1ff91a8b65293df69f062d91ea
visheshdubey <visheshdubey2016@gmail.com>
2024-11-27 10:29:13 +0530

    feat: added KpiCard

---
+++ features/transaction/components/KpiCard.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+
+type Props = {
+    label: string;
+    value: string;
+    variant?: "default" | "danger";
+    className?: string;
+};
+
+const KpiCard = ({ label, value, variant = "default", className }: Props) => {
+    return (
+        <div className={cn("flex px-6 border-r flex-col gap-4", className)}>
+            <p className="text-xs font-semibold text-neutral-500">{label}</p>
+            <p className={`text-5xl  font-black ${variant === "danger" ? "text-red-800" : "text-neutral-800"}`}>{value}</p>
+        </div>
+    );
+};
+
+export default KpiCard;
