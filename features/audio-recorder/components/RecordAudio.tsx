f1c69c38b3980d296010cf859f02602f4b57a373
visheshdubey <visheshdubey2016@gmail.com>
2024-12-01 12:51:37 +0530

    feat: made UI adjustments acc to recorder states + hooked apis

--- features/audio-recorder/components/RecordAudio.tsx
+++ features/audio-recorder/components/RecordAudio.tsx
@@ -1,28 +1,59 @@
 "use client";
 
 import { Check, Mic } from "lucide-react";
-import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger,
-} from "@/components/ui/drawer";
+import { Cross2Icon, StopIcon } from "@radix-ui/react-icons";
+import { Drawer, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import { ReactNode, useState } from "react";
-import { Recorder, RecorderError, RecorderTime, RecorderTrigger, RecorderWaveform } from "@/components/primitives/ui/record-audio";
+import {
+    Recorder,
+    RecorderDiscard,
+    RecorderError,
+    RecorderTime,
+    RecorderTrigger,
+    RecorderWaveform,
+    RecordingState,
+} from "@/components/primitives/ui/record-audio";
 
 import { Button } from "@/components/ui/button";
-import { Cross2Icon } from "@radix-ui/react-icons";
+import { ButtonAsync } from "@/components/primitives/ui/button-async";
+import { useSpeechToText } from "../hooks/useSpeechToText";
+import { useTextToJson } from "../hooks/useTextToJson";
 
 export function AudioRecorderWidget({ children }: { children: ReactNode }) {
     const [audioBlob, setaudioBlob] = useState<Blob | null>();
+    const [transcribedText, setTranscribedText] = useState<{ text: string } | null>(null);
+    const [recordingState, setRecordingState] = useState<RecordingState>("idle");
+    const { mutate: transcribeAudio, isPending } = useSpeechToText({
+        onSuccess(text) {
+            setTranscribedText(text);
+            console.log(text);
+        },
+    });
+    const { mutate: convertTextToJson, isPending: isConvertingTextToJson } = useTextToJson({
+        onSuccess(data) {
+            console.log(data);
+        },
+    });
 
     const handleComplete = async (blob: Blob | null) => {
         setaudioBlob(blob);
+
+        if (blob) {
+            transcribeAudio(blob);
+        }
+
         console.log("Recording completed, blob size:", blob?.size);
+    };
+
+    const handleApproveTranscription = async () => {
+        if (transcribedText) {
+            convertTextToJson(transcribedText);
+        }
+    };
+
+    const handleDiscard = () => {
+        setTranscribedText(null);
+        setRecordingState("idle");
     };
 
     return (
@@ -35,30 +66,64 @@ export function AudioRecorderWidget({ children }: { children: ReactNode }) {
                         <DrawerDescription className="text-center">"Spent 12000 USD on cat food".</DrawerDescription>
                     </DrawerHeader>
                     <div className="p-4 pb-0">
-                        <Recorder maxDuration={30} onComplete={handleComplete} onRecChange={(state) => console.log(state)} className="">
+                        <Recorder
+                            recordingState={recordingState}
+                            onStateChange={setRecordingState}
+                            maxDuration={5}
+                            onComplete={handleComplete}
+                            onRecChange={(state) => console.log(state)}
+                        >
                             <RecorderError />
 
                             <div className="flex flex-col items-center">
-                                <RecorderWaveform className="flex-1" barCount={60} />
-
+                                {transcribedText ? (
+                                    <span className="text-center h-[50px] text-sm text-neutral-500 font-medium">
+                                        {transcribedText.text}
+                                    </span>
+                                ) : (
+                                    <RecorderWaveform className="flex-1" barCount={60} />
+                                )}
                                 <RecorderTime className="text-center font-medium" />
                                 <div className="flex items-center gap-4 mt-8">
-                                    {audioBlob ? (
-                                        <DrawerClose asChild>
-                                            <Button size="lg" variant="outline" className="rounded-full w-12 h-12 p-0">
+                                    {recordingState === "completed" || recordingState === "stopped" ? (
+                                        <RecorderDiscard asChild>
+                                            <Button
+                                                size="lg"
+                                                onClick={handleDiscard}
+                                                variant="outline"
+                                                className="rounded-full w-12 h-12 p-0"
+                                            >
                                                 <Cross2Icon className="h-4 w-4" />
                                             </Button>
-                                        </DrawerClose>
+                                        </RecorderDiscard>
                                     ) : null}
                                     <RecorderTrigger asChild>
-                                        <Button size="lg" variant="default" className="rounded-full w-12 h-12 p-0">
-                                            <Mic className="h-4 w-4" />
-                                        </Button>
+                                        <ButtonAsync
+                                            variant={recordingState === "recording" ? "outline" : "default"}
+                                            size="lg"
+                                            className="rounded-full w-12 h-12 p-0 relative"
+                                            spinnerClassName="absolute inset-0 w-full h-full mx-auto my-auto"
+                                            isLoading={isPending}
+                                        >
+                                            {!isPending && (
+                                                <>
+                                                    {recordingState !== "recording" && <Mic className="h-4 w-4" />}
+                                                    {recordingState === "recording" && <StopIcon className="h-4 w-4" />}
+                                                </>
+                                            )}
+                                        </ButtonAsync>
                                     </RecorderTrigger>
-                                    {audioBlob ? (
-                                        <Button size="lg" variant="secondary" className="rounded-full w-12 h-12 p-0">
-                                            <Check className="h-4 w-4" />
-                                        </Button>
+                                    {recordingState === "completed" || recordingState === "stopped" ? (
+                                        <ButtonAsync
+                                            onClick={handleApproveTranscription}
+                                            isLoading={isConvertingTextToJson}
+                                            size="lg"
+                                            variant="secondary"
+                                            className="rounded-full w-12 h-12 p-0 relative"
+                                            spinnerClassName="absolute inset-0 w-full h-full mx-auto my-auto"
+                                        >
+                                            {!isConvertingTextToJson && <Check className="h-4 w-4" />}
+                                        </ButtonAsync>
                                     ) : null}
                                 </div>
 
