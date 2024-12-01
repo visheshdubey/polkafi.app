b572a6ef0b310ae3882106b22583c769b4c3064e
visheshdubey <visheshdubey2016@gmail.com>
2024-12-02 00:07:09 +0530

    fix: signin form margins

--- features/auth/components/SignInForm.tsx
+++ features/auth/components/SignInForm.tsx
@@ -15,7 +15,7 @@ const SignInForm = ({ providers }: Props) => {
     return (
         <div className="max-w-lg flex flex-col mx-auto px-4 grow h-full w-full items-center justify-center">
             <div className="glass-card-gradient z-20 max-w-md w-full min-h-[300px] rounded-md  border border-neutral-100 p-8 flex items-center justify-center flex-col gap-4">
-                <h1 className="text-sm text-center mb-8 text-slate-800/70">
+                <h1 className="text-sm text-center mb-3 text-slate-800/70">
                     <p className="mb-2">Welcome to </p>
                     <BrandLogo className="text-2xl font-bold"></BrandLogo>
                 </h1>
